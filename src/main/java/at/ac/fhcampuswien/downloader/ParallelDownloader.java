a0898d18278971ba2e2946f2c2ff3411ed83df25
IshakSij <92020454+IshakSij@users.noreply.github.com>
2022-06-26 18:48:23 +0200

    26.06.
    AppController:
    added stream to extract urls from articles
    
    ParallelDownloader:
    added download function using multiple threads

--- src/main/java/at/ac/fhcampuswien/downloader/ParallelDownloader.java
+++ src/main/java/at/ac/fhcampuswien/downloader/ParallelDownloader.java
@@ -1,6 +1,8 @@
 package at.ac.fhcampuswien.downloader;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
 
 // Class is needed for exercise 4 - ignore for exercise 3 solution
 public class ParallelDownloader extends Downloader{
@@ -10,6 +12,33 @@ public class ParallelDownloader extends Downloader{
     public int process(List<String> urls) {
         // TODO implement download function using multiple threads
         // Hint: use ExecutorService with Callables
-        return 0;
+        /*
+          https://www.baeldung.com/java-runnable-callable
+         */
+
+        int amountOfDownloads = 0;
+        int threadCounter = Runtime.getRuntime().availableProcessors();
+        ExecutorService threadPool = Executors.newFixedThreadPool(threadCounter);
+
+        List<Callable<String>> callables = new ArrayList<>();
+
+        for (String url: urls) {
+            Callable<String> download = () -> saveUrl2File(url);
+            callables.add(download);
+        }
+
+        try {
+            List<Future<String>> totalFutureDownloads = threadPool.invokeAll(callables);
+            for (Future<String> future: totalFutureDownloads) {
+                if (future.get() != null) {
+                    amountOfDownloads++;
+                }
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            System.out.println(e.getMessage());
+        }
+
+        threadPool.shutdown();
+        return amountOfDownloads;
     }
 }
